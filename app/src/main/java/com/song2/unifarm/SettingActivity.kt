3c2b87e2088050056475c087a8f5c975865eb9fb
KimMooHyeon <dlfb77@gmail.com>
2019-09-06 12:42:50 +0900

    plus font

--- app/src/main/java/com/song2/unifarm/SettingActivity.kt
+++ app/src/main/java/com/song2/unifarm/SettingActivity.kt
@@ -2,11 +2,17 @@ package com.song2.unifarm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        //뒤로가기
+        rl_setting_act_back_btn.setOnClickListener {
+            finish()
+        }
+
     }
 }
